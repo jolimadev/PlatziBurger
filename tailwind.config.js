a1b0c177fa2004b3758a79776485d518d89e713e
olimajs <jsapps@outlook.es>
2021-12-07 17:09:08 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+        'secondary': '#353535'
+      },
+      height: {
+        '80':'20 rem',
+        '100':'25 rem'
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
